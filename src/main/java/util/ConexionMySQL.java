98944259dadbfefce572eafa4c8d657d1b1878c1
Btlguse <javier.luna@ug.edu.ec>
2025-10-07 00:47:54 -0500

    Change MySQL connection credentials to empty
    Updated database connection credentials to empty strings.

--- src/main/java/util/ConexionMySQL.java
+++ src/main/java/util/ConexionMySQL.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class ConexionMySQL {
     private static final String URL = "jdbc:mysql://localhost:3306/travel_agency_g8";
     //private static final String URL = "jdbc:mysql://localhost:3306/test_agency_g8";
-    private static final String USER = "root";
-    private static final String PASSWORD = "toor";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         try {
